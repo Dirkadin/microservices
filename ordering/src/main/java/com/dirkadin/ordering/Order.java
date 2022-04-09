f73e8f596104c32753ce8ee33244d464690cd74f
Dirk <dirkadin@gmail.com>
2022-04-08 20:56:21 -0500

    Made Order an entity

--- ordering/src/main/java/com/dirkadin/ordering/Order.java
+++ ordering/src/main/java/com/dirkadin/ordering/Order.java
@@ -1,12 +1,33 @@
 package com.dirkadin.ordering;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ordering") //order is a protected table name by postgres
 @Builder(toBuilder = true)
 public class Order {
 
+  @Id
+  @SequenceGenerator(
+      name = "ordering_id_sequence",
+      sequenceName = "ordering_id_sequence"
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "ordering_id_sequence"
+  )
   private int orderId;
   private int productId;
   private int quantity;
