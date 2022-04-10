348134ea964b8a83ce365ed011be347f916de102
Dirk <dirkadin@gmail.com>
2022-04-09 22:05:19 -0500

    Initial commit of inventory service

---
+++ inventory/src/main/java/com/dirkadin/inventory/InventoryController.java
@@ -0,0 +1,19 @@
+package com.dirkadin.inventory;
+
+import com.dirkadin.clients.inventory.InventoryCheckResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/inventory")
+public class InventoryController {
+
+  @GetMapping
+  public InventoryCheckResponse checkInventory() {
+    return null;
+  }
+
+}
