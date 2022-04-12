906ecc6e17889928d3bd10112677cfa208f75a2b
Dirk <dirkadin@gmail.com>
2022-04-12 15:27:44 -0500

    Fixed formatting

--- ordering/src/main/java/com/dirkadin/ordering/OutOfStockException.java
+++ ordering/src/main/java/com/dirkadin/ordering/OutOfStockException.java
@@ -1,6 +1,6 @@
 package com.dirkadin.ordering;
 
-public class OutOfStockException extends RuntimeException{
+public class OutOfStockException extends RuntimeException {
 
   public OutOfStockException(String message) {
     super(message);
