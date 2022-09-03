bb57e816dd50b1ca3a5086041f3f8ebcc4236733
Dirk <dirkadin@gmail.com>
2022-09-03 10:03:42 -0500

    Creating rabbitmq exchange, queue, and binding them together

--- ordering/src/main/java/com/dirkadin/ordering/OrderingConfig.java
+++ ordering/src/main/java/com/dirkadin/ordering/OrderingConfig.java
@@ -1,7 +1,12 @@
 package com.dirkadin.ordering;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
@@ -16,4 +21,22 @@ public class OrderingConfig {
 
   @Value(("${rabbitmq.routing-keys.internal-ordering"))
   private String internalOrderingRoutingKey;
+
+  @Bean
+  public TopicExchange internalTopicExchange() {
+    return new TopicExchange(this.internalExchange);
+  }
+
+  @Bean
+  public Queue orderingQueue() {
+    return new Queue(this.orderingQueue);
+  }
+
+  @Bean
+  public Binding internalToOrderingBinding() {
+    return BindingBuilder
+        .bind(orderingQueue())
+        .to(internalTopicExchange())
+        .with(this.internalOrderingRoutingKey);
+  }
 }
