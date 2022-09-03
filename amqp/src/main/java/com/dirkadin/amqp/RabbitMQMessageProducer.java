d10812c9a8a8926cb094c68e2dff2f665eb3a0cd
Dirk <dirkadin@gmail.com>
2022-09-03 11:24:13 -0500

    Added simple logging statements to producer

--- amqp/src/main/java/com/dirkadin/amqp/RabbitMQMessageProducer.java
+++ amqp/src/main/java/com/dirkadin/amqp/RabbitMQMessageProducer.java
@@ -1,9 +1,11 @@
 package com.dirkadin.amqp;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class RabbitMQMessageProducer {
@@ -11,6 +13,8 @@ public class RabbitMQMessageProducer {
   private final AmqpTemplate amqpTemplate;
 
   public void publish(Object payload, String exchange, String routingKey) {
+    log.info("Publishing to {} using routingkey {}. Payload: {}", exchange, routingKey, payload);
     amqpTemplate.convertAndSend(exchange, routingKey, payload);
+    log.info("Published to {} using routingkey {}. Payload: {}", exchange, routingKey, payload);
   }
 }
