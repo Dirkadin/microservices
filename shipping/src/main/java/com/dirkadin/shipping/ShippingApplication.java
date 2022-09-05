c422d097411d26b99222b6f2a10bc8836e15713c
Dirk <dirkadin@gmail.com>
2022-09-04 20:24:41 -0500

    Added dependencies and package scan to shipping service

--- shipping/src/main/java/com/dirkadin/shipping/ShippingApplication.java
+++ shipping/src/main/java/com/dirkadin/shipping/ShippingApplication.java
@@ -3,7 +3,10 @@ package com.dirkadin.shipping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.dirkadin.shipping",
+    "com.dirkadin.amqp"
+})
 public class ShippingApplication {
 
   public static void main(String[] args) {
